9183afc74f5dde41037039399bb42a3565505031
Sinnmayer <nicolas.sinnmayer@htl-wels.at>
2025-11-09 19:15:15 +0100

    Mehtoden, Konstukktor und Setter, getter

--- src/Student.java
+++ src/Student.java
@@ -5,4 +5,82 @@ public class Student {
     private String department;
     private int year;
     private String name;
+
+    //KONSTRUKTOREN
+
+    public Student() {
+
+    }
+
+    public Student (String name, String dateOfBirth, char sex) {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.sex = sex;
+    }
+
+    public Student (char sex, String dateOfBirth, String department, int year, String name) {
+        this.sex = sex;
+        this.dateOfBirth = dateOfBirth;
+        this.department = department;
+        this.year = year;
+        this.name = name;
+    }
+
+    //GETTER SETTER
+
+    //sex
+    public char getSex() {
+        return sex;
+    }
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+    //dateofbirth
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+    //department
+    public String getDepartment() {
+        return department;
+    }
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+    //Year
+    public int getYear() {
+        return year;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+    //name
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //METHODEN
+
+    public void raiseYear(int years) {
+        setYear(getYear() + years);
+
+        if (getYear() > 5) setYear(5);
+    }
+
+    public boolean checkStats(char sex, String dateOfBirth, String department, int year, String name) {
+        if ((sex == getSex())
+                && (dateOfBirth.equalsIgnoreCase(getDateOfBirth()))
+                && (department.equalsIgnoreCase(getDepartment()))
+                && (year == getYear())
+                && (name.equalsIgnoreCase(getName()))) {
+            return true;
+        } else return false;
+    }
+
+
 }
--- src/Teacher.java
+++ src/Teacher.java
@@ -1,7 +1,38 @@
 public class Teacher {
 
     private char sex;
-    private int dateOfBirth;
+    private String dateOfBirth;
     private String department;
     private String name;
+
+    //GETTER SETTER
+
+    //sex
+    public char getSex() {
+        return sex;
+    }
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+    //dateofbirth
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+    //department
+    public String getDepartment() {
+        return department;
+    }
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+    //name
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 }
